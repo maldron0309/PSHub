9b421cb4f58102556a3237d4b327e487c2fc78bc
maldron <103731019+maldron0309@users.noreply.github.com>
2023-09-27 10:56:17 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+	double a, b;
+	
+	cin >> a >> b;
+
+	cout << fixed;
+	cout.precision(9);
+
+	cout << a / b << endl;
+
+	return 0;
+}
