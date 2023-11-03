39d0bf7b0cc14293eebfc97ef6a1bee81094be12
maldron <bagjinhyeong640@gmail.com>
2023-11-03 15:00:36 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main() {
+	
+	int grade;
+
+	scanf("%d", &grade);
+
+	if (grade >= 90)
+	{
+		printf("A");
+	}
+	else if (grade >= 80)
+	{
+		printf("B");
+	}
+	else if (grade >= 70)
+	{
+		printf("C");
+	}
+	else if (grade >= 60)
+	{
+		printf("D");
+	}
+	else
+	{
+		printf("F");
+	}
+
+	return 0;
+
+}
+
