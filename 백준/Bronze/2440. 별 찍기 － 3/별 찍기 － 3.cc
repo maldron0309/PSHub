93e2d1cf75c353c391384173772ef883f2fe7ec5
Maldron <bagjinhyeong640@gmail.com>
2024-04-10 10:21:17 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    
+	int n = 0;
+
+	cin >> n;
+
+	for (int i = 0; i <= n; i++)
+	{
+		for (int j = 0; j < n-i; j++)
+		{
+			cout << "*";
+		}
+
+		cout << '\n';
+	}
+    return 0;
+}
