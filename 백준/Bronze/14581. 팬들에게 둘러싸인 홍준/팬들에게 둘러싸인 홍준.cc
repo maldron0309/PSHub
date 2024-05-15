9e32e21af78214fe93efefa97a4ef815591dcd3f
Maldron7 <bagjinhyeong640@gmail.com>
2024-05-16 08:32:36 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string id;
+	string fan = ":fan:";
+	cin >> id;
+
+	cout << fan << fan << fan << '\n';
+	cout << fan << ":" << id << ":" << fan << '\n';
+	cout << fan << fan << fan;
+	return 0;
+}
