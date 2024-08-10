54fafeb282616b76ea14be4c8265df1a7a764dc8
Maldron <bagjinhyeong640@gmail.com>
2024-08-10 14:28:56 +0900

    [Silver V] Title: 다리 놓기, Time: 12 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.cc
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -1,32 +1,30 @@
-#include <bits/stdc++.h>
-#define endl '\n'
-using namespace std;
+#include <iostream>
+#include <array>
+#include <algorithm>
 
-array<array<int, 30>, 30> memo;
+std::array<std::array<int, 30>, 30> memo{};
 
-int combination(int n, int r) {
+int Combination(int n, int r) 
+{
     if (memo[n][r]) return memo[n][r];
-    else{
-        memo[n][r] = combination(n - 1, r - 1) + combination(n - 1, r);
-        return memo[n][r];
-    }
+
+    return memo[n][r] = Combination(n - 1, r - 1) + Combination(n - 1, r);
 }
 
-int main () {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
- 
-    for(auto& x : memo) fill(x.begin(), x.end(), 0);
-    for(int i = 0; i < 30; i++) {
+int main()
+{
+    for (auto& x : memo) std::fill(x.begin(), x.end(), 0);
+    for (int i = 0; i < 30; i++) {
         memo[i][0] = memo[i][i] = 1;
     }
 
-    int tcase, n, r;
+    int tcase{}, n{}, r{};
+    std::cin >> tcase;
 
-    cin >> tcase;
-    while(tcase--){
-        cin >> r >> n;
-        cout << combination(n, r) << endl;
+    while (tcase--) {
+        std::cin >> r >> n;
+        std::cout << Combination(n, r) << '\n';
     }
+
+    return 0;
 }
