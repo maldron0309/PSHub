69b46270c23b3ebe3fc4abdf42c25e4d51a22520
Maldron <bagjinhyeong640@gmail.com>
2024-08-10 15:34:51 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,29 +1,32 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std;
 
-int main() {
-    vector<int> v(9);
-    int totalHeight = 0;
+int main()
+{
+    std::vector<int> v(9);
+    int totalHeight{ 0 };
 
-    for (int i = 0; i < 9; i++) {
-        cin >> v[i];
+    for (int i{ 0 }; i < 9; ++i)
+    {
+        std::cin >> v[i];
         totalHeight += v[i];
     }
 
-    sort(v.begin(), v.end());
+    std::sort(v.begin(), v.end());
 
-    int overHeight = totalHeight - 100; 
+    int overHeight{ totalHeight - 100 };
 
-    for (int i = 0; i < 8; i++) {
-        for (int j = i + 1; j < 9; j++) {
-            if (v[i] + v[j] == overHeight) {
-                v.erase(v.begin() + j); // 먼저 j번째 난쟁이를 제거
-                v.erase(v.begin() + i); // 그 다음 i번째 난쟁이를 제거
-                for (int k = 0; k < 7; k++) {
-                    cout << v[k] << '\n';
-                }
+    for (int i{ 0 }; i < 8; ++i)
+    {
+        for (int j{ i + 1 }; j < 9; ++j)
+        {
+            if (v[i] + v[j] == overHeight)
+            {
+                v.erase(v.begin() + j);
+                v.erase(v.begin() + i);
+                for (int k{ 0 }; k < 7; ++k)
+                    std::cout << v[k] << '\n';
                 return 0;
             }
         }
