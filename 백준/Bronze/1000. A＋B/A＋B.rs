ee8d3340b666a46b7a6a9dc48cd9be6f35373778
Maldron <bagjinhyeong640@gmail.com>
2024-09-12 11:38:19 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 13208 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.rs
+++ 백준/Bronze/1000. A＋B/A＋B.rs
@@ -6,7 +6,6 @@ fn main() {
     io::stdin().read_line(&mut input).unwrap();
 
     let values:Vec<i32> = input
-        .as_mut_str()
         .split_whitespace()
         .map(|input| input.parse().unwrap())
         .collect();
